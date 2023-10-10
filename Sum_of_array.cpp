571187bc20a1044dbafa660a2101868e3a3cd381
-Ramendra <121676584+rpy9621@users.noreply.github.com>
2023-10-10 21:51:46 +0530

    Update Sum_of_array.cpp

--- Sum_of_array.cpp
+++ Sum_of_array.cpp
@@ -1,1 +1,11 @@
 #include<iostream>
+using namespace std;
+int main() {
+  int n;
+  cout<<"Enter size of the array:\n";
+  cin>>n;
+  int sum=0;
+  int array[n];
+  for(i=0;i<n;i++) {
+  cin>>arr[i];
+}
