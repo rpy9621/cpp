a922e7718b8de3afb60818eaf9bcff79465cffe8
-Ramendra <121676584+rpy9621@users.noreply.github.com>
2023-10-11 22:42:23 +0530

    Update Sum_of_array.cpp

--- Sum_of_array.cpp
+++ Sum_of_array.cpp
@@ -1,11 +1,17 @@
 #include<iostream>
 using namespace std;
 int main() {
-  int n;
+  int n,i;
   cout<<"Enter size of the array:\n";
   cin>>n;
   int sum=0;
   int array[n];
+  cout<<"Enter "<<n<<" element's in array:\n";
   for(i=0;i<n;i++) {
   cin>>arr[i];
+}
+  for(i=0;i<n;i++){
+  sum=sum+arr[i];
+  }
+  cout<<"Sum of array rlrmrnt's is: "<<sum;
 }
