2f4c3b9d5a911eee0627068dd616b51101bada42
Ramendra <121676584+rpy9621@users.noreply.github.com>
2024-02-26 19:41:42 +0530

    Fibonacci_Series_via_array.cpp

--- Fibonacci_Series_via_array.cpp
+++ Fibonacci_Series_via_array.cpp
@@ -1,20 +1,26 @@
 #include<iostream>
 using namespace std;
-int Fibonaci(int arr[],int n){
-  arr[0]=0;
-  arr[1]=1;
-  cout<<"Fibonacci Series using array is :\n";
-  for(int i=2;i<n;i++){
-  arr[i]=arr[i-2]+arr[i-1];
-  }
-  cout<<"Fibonacci series upto "<<n<<" terms is :\n";
-  for(int i=0;i<n;i++){
-  fib[i];
-  }
-int main(){
-  int n;
-  cout<<"Enter number of term's :\n";
-  cin>>n;
-  Fibonaci(n);
-  return 0;
+
+void Fibonacci(int arr[], int n) {
+    arr[0] = 0;
+    arr[1] = 1;
+    cout << "Fibonacci Series using array is :\n";
+    for(int i = 2; i < n; i++) {
+        arr[i] = arr[i - 2] + arr[i - 1];
+    }
+    cout << "Fibonacci series upto " << n << " terms is :\n";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cout << "Enter number of terms:\n";
+    cin >> n;
+    int arr[n];
+    Fibonacci(arr, n);
+    return 0;
+}
 }
